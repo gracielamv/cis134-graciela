7c26fabd4416f8be727374c6ddaa98f96c68f15c
gracielamv <gracenoemi2006@gmail.com>
2026-05-12 11:17:09 -0500

    small homepage changes

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ if (x.className === "") {
     x.className = ""; 
 } 
 } 
+
+let slideIndex = 0;
+showSlides();
+
+// https://www.w3schools.com/howto/howto_js_slideshow.asp
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 10000);
+}
